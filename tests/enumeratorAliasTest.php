e5e4788d0e600bab8e3be1e76d9968cb4bb5855a
Blaine Schmeisser <BlaineSch@gmail.com>
2012-07-08 00:25:13 -0500

    Added files for phpunit

---
+++ tests/enumeratorAliasTest.php
@@ -0,0 +1,7 @@
+<?php
+
+// All tests for method aliases should be in here.
+
+class enumeratorAliasTest extends PHPUnit_Framework_TestCase {
+
+}
---
+++ tests/prettyArrayTests.php
@@ -0,0 +1,7 @@
+<?php
+
+// All tests for pretty array should be in here.
+
+class prettyArrayTests extends PHPUnit_Framework_TestCase {
+
+}
